4a3332e0b2f3590cc961a5003bc9dc61dbb11d0a
jian <p000207jian@gmail.com>
2024-08-24 10:51:05 +0900

    feat: 9093

--- 1st/9093.py
+++ 1st/9093.py
@@ -3,16 +3,4 @@ import sys
 n = int(sys.stdin.readline())
 
 for _ in range(n):
-    string = sys.stdin.readline().strip() + ' '
-    stack = []
-    result = []
-    
-    for char in string:
-        if char == ' ':
-            while stack:
-                result.append(stack.pop())
-            result.append(' ')
-        else :
-            stack.append(char)
-            
-    print(''.join(result))
+    print(' '.join(list(map(lambda s: ''.join(list(reversed(list(s)))), sys.stdin.readline().strip().split(' ')))))
